0e7b1ef541a3676a5308c794fc4c4166a4693492
ibrahimbott <amreliwoods@gmail.com>
2025-12-30 00:15:49 +0500

    wwwsa

--- web-app/next.config.js
+++ web-app/next.config.js
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  trailingSlash: true,
+  // trailingSlash: true,
   images: {
     unoptimized: true
   },
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:8000/api/:path*',
-      },
-    ]
+    const isDev = process.env.NODE_ENV === 'development'
+    if (isDev) {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:8000/api/:path*',
+        },
+      ]
+    }
+    return []
   }
 }
 
